75d97bb2504964d6830c003a4519a71adb75ae71
Bryndye <121563935+HugoCluclu@users.noreply.github.com>
2023-05-12 15:25:11 +0200

    Camera Controller

--- Assets/_Scripts/Player/CameraController.cs
+++ Assets/_Scripts/Player/CameraController.cs
@@ -4,9 +4,52 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Transform anchorCamera;
+    [SerializeField] private Transform mainCamera;
+
+    [Header("Mvt Horizontal")]
+    [SerializeField] private float rotationSpeed = 70f;
+
+    [Header("Mvt Vertical")]
+    [SerializeField] private float forwardSpeed = 50f;
+    [SerializeField] private float minDistance = -5f;
+    [SerializeField] private float maxDistance = -20f;
 
     void Update()
     {
-        
+        cameraMouvementHorizontal();
+        cameraMovementForward();
     }
+
+    private void cameraMouvementHorizontal()
+    {
+        float _axeX = Input.GetAxis("Horizontal");
+
+        if (_axeX == 0)
+        {
+            return;
+        }
+        float rotationAmount = -_axeX * rotationSpeed * Time.deltaTime;
+        Vector3 currentRotation = transform.eulerAngles;
+        Vector3 newRotation = new Vector3(currentRotation.x, currentRotation.y + rotationAmount, currentRotation.z);
+        transform.eulerAngles = newRotation;
+    }
+
+    private void cameraMovementForward()
+    {
+        float _axisZ = Input.GetAxis("Vertical");
+        if (_axisZ == 0)
+        {
+            return;
+        }
+
+        float movementAmount = _axisZ * forwardSpeed * Time.deltaTime;
+
+        // Déplace la caméra sur son axe Z
+        Vector3 cameraPosition = mainCamera.localPosition;
+        float newZPosition = Mathf.Clamp(cameraPosition.z + movementAmount, maxDistance, minDistance);
+        Vector3 newPosition = new Vector3(cameraPosition.x, cameraPosition.y, newZPosition);
+        mainCamera.localPosition = newPosition;
+    }
+
 }
--- Assets/_Scripts/Tiles/Tile.cs
+++ Assets/_Scripts/Tiles/Tile.cs
@@ -2,7 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType
+{
+    NoEvent,
+    RandomEvent,
+    Shop
+}
+
+public enum TileTState
+{
+    Unused,
+    Used
+}
 public class Tile : MonoBehaviour
 {
+    public TileType MyType;
+    public TileTState MyState = TileTState.Unused;
     public Vector2 MyPosition;
 }
