c7130b2ca8de488edbf6950031a033751a5debcd
Bryndye <121563935+HugoCluclu@users.noreply.github.com>
2023-05-12 14:28:18 +0200

    Tilemap

---
+++ Assets/_Scripts/Tiles/TileEditor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Tile))]
+public class TileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Update Positions"))
+        {
+            Tile[] tiles = FindObjectsOfType<Tile>();
+
+            int width = 8;
+            int height = 8;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int index = y * width + x;
+                    if (index < tiles.Length)
+                    {
+                        tiles[index].MyPosition = new Vector2(x, y);
+                        EditorUtility.SetDirty(tiles[index]);
+                    }
+                }
+            }
+        }
+    }
+}
